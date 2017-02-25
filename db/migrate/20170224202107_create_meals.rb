044f82a20c6d41cad0229af6c637aa5d6466ca2e
Erica Norby <ericanorby@gmail.com>
2017-02-25 18:28:06 -0500

    initial commit

---
+++ db/migrate/20170224202107_create_meals.rb
@@ -0,0 +1,13 @@
+class CreateMeals < ActiveRecord::Migration[5.0]
+  def change
+    create_table :meals do |t|
+      t.string :name
+      t.string :photo_url
+      t.integer :servings
+      t.decimal :total_cost
+      t.decimal :cost_per_serving
+      t.string :difficulty
+      t.string :directions
+    end
+  end
+end
---
+++ db/migrate/20170224202121_create_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateIngredients < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.decimal :amount
+      t.string :measurement
+      t.integer :calories
+      t.references :meal
+    end
+  end
+end
