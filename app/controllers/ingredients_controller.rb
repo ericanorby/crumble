70d234de162f66aaa4963789bd8aa405aea675e6
Erica Norby <ericanorby@gmail.com>
2017-02-26 11:00:35 -0500

    can create new meals and ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,20 @@
 class IngredientsController < ApplicationController
+
+  def new
+    @meal = Meal.find(params[:meal_id])
+    @ingredient = @meal.ingredients.new
+  end
+
+  def create
+    @meal = Meal.find(params[:meal_id])
+    @ingredient = @meal.ingredients.create(ingredient_params)
+    redirect_to meal_path(@meal)
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :amount, :measurement, :calories)
+  end
+
 end
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,6 +6,22 @@ class MealsController < ApplicationController
 
   def show
     @meal = Meal.find(params[:id])
+    @ingredients = @meal.ingredients.all
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.create(meal_params)
+    redirect_to meal_path(@meal)
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :photo_url, :servings, :total_cost, :difficulty, :directions)
   end
 
 end
